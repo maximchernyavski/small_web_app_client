a47d1049d6707d553b3cc2ba7b627130b8c18d1d
maximchernyavski <maxim.chernyavski2@gmail.com>
2024-10-24 15:19:12 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="menu">
+        <div id="logo_box">
+            <div id="logo">WEB_SHIT</div>
+        </div>
+        <input
+            id="login"
+            class="input"
+            placeholder="User ID"
+            type="text"
+        />
+        <input
+            id="password"
+            class="input"
+            placeholder="Password"
+            type="text"
+        />
+    </div>
+  );
+}
+
+export default App;
